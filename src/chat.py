ef4f314bb9b345e5acea0a614b4a1f42380b27a0
Youichi Fujimoto <yofujimo@gmail.com>
2025-03-15 22:23:59 +0900

    feat: [langgraph] summary memory

--- src/chat.py
+++ src/chat.py
@@ -3,7 +3,7 @@ from typing import Protocol
 
 from langchain_anthropic import ChatAnthropic
 from langchain_core.language_models import BaseChatModel
-from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.messages import HumanMessage, RemoveMessage, SystemMessage
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import START, MessagesState, StateGraph
 
@@ -28,10 +28,50 @@ class LangGraphChatAgent:
         workflow = StateGraph(state_schema=MessagesState)
 
         # Define the function that calls the model
+        # def call_model(state: MessagesState):
+        #     messages = [SystemMessage(content=system_prompt)] + state["messages"]
+        #     response = model.invoke(messages)
+        #     return {"messages": response}
         def call_model(state: MessagesState):
-            messages = [SystemMessage(content=system_prompt)] + state["messages"]
-            response = model.invoke(messages)
-            return {"messages": response}
+            system_message = SystemMessage(content=system_prompt)
+            message_history = state["messages"][
+                :-1
+            ]  # exclude the most recent user input
+            # Summarize the messages if the chat history reaches a certain size
+            if len(message_history) >= 4:
+                last_human_message = state["messages"][-1]
+                # Invoke the model to generate conversation summary
+                summary_prompt = (
+                    "Distill the above chat messages into a single summary message. "
+                    "Include as many specific details as you can."
+                )
+                summary_message = model.invoke(
+                    message_history + [HumanMessage(content=summary_prompt)]
+                )
+
+                # Delete messages that we no longer want to show up
+                delete_messages = [RemoveMessage(id=m.id) for m in state["messages"]]
+                # Re-add user message
+                human_message = HumanMessage(content=last_human_message.content)
+                # Call the model with summary & response
+                logger.debug(
+                    f"Calling model with {system_message}, {summary_message}, {human_message}"
+                )
+                response = model.invoke(
+                    [system_message, summary_message, human_message]
+                )
+                message_updates = [
+                    summary_message,
+                    human_message,
+                    response,
+                ] + delete_messages
+            else:
+                logger.debug(
+                    f"Calling model with {system_message} and messages: {state['messages']}"
+                )
+                message_updates = model.invoke([system_message] + state["messages"])
+
+            return {"messages": message_updates}
 
         # Define the node and edge
         workflow.add_node("model", call_model)
@@ -61,7 +101,7 @@ class LangGraphChatAgent:
 class AnthropicChatAgent(LangGraphChatAgent):
     def __init__(self, api_key="", model_name="", lang="en", thread_id=""):
         self.llm = ChatAnthropic(
-            api_key=api_key, model_name=model_name, max_tokens=1000
+            api_key=api_key, model_name=model_name, max_tokens=10000
         )
         super().__init__(self.llm, lang, thread_id)
 
